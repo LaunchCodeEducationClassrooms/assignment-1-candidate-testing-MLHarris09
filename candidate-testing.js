eefa1af7f3fe19d53217580a04e59fa919e73c4f
MLHarris <marylara@gmail.com>
2021-02-21 05:42:45 +0000

    more updates

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,7 +10,7 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ", "True or false: 5000 meters == 5 kilometers? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "]
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3",]
-let candidateAnswers = "";
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -21,19 +21,20 @@ candidateName = input.question("Please enter your name:");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-for (i = 0; i < questions.length; i++) {
-  candidateAnswers[i] = input.question(questions[i]); 
-}
-
-
+for (let i = 0; i < questions.length; i++){
+    candidateAnswers[i] = input.question(questions[i]);
+  }
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log(`Your answer is ${candidateAnswer[i]}`); 
-console.log(`The correct answer is ${correctAnswers[i]}`);
-
+for (let i = 0; i < questions.length; i++){
+  if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+    numCorrect++;
+  }
+console.log(`${i + 1} ${questions[i]} \n Your answer: ${candidateAnswers[i]} \n Correct answer: ${correctAnswers[i]}`)
+}
   let grade;
   
 
@@ -43,7 +44,7 @@ console.log(`The correct answer is ${correctAnswers[i]}`);
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  console.log("Welcome! " + candidateName);
+  console.log(`Welcome!  ${candidateName}`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
